4b40450c6e09c780081d569b2fade1e855095647
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-01-15 10:31:57 +0700

    fix error

--- submit/algo-design/generative/dsa01021/dsa01021.cpp
+++ submit/algo-design/generative/dsa01021/dsa01021.cpp
@@ -16,6 +16,7 @@ void testCase() {
     int n, k;
     cin >> n >> k;
     int *x = new int[n + 1];
+    int *a = new int[n + 1];
     for (int i = 1; i <= k; i++) {
         cin >> x[i];
     }
@@ -23,6 +24,31 @@ void testCase() {
     while (t > 0 && x[t] == (n - k + t)) {
         t--;
     }
-    cout << (k - t);
+    if (t == 0) {
+        cout << k;
+    } else {
+        for (int i = 0; i < t; i++) {
+            a[i] = x[i];
+        }
+        a[t] = x[t] + 1;
+        for (int i = t + 1; i <= k; i++) {
+            a[i] = a[i - 1] + 1;
+        }
+        int count = 0;
+        for (int i = 1; i <= k; i++) {
+            // cout << "a[i]: " << a[i]  << ", x[i]: " << x[i] << endl;
+            bool ok = false;
+            for (int j = 1; j <= k; j++) {
+                if (a[j] == x[i]) {
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                count++;
+            }
+        }
+        cout << count;
+    }
 
 }
