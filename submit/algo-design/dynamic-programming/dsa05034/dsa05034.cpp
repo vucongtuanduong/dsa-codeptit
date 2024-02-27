9e45d2aa17e281068a1710dee6f2e41c2265364f
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-02-27 16:18:08 +0700

    [DYNAMIC PROGRAMMIGN]

--- submit/algo-design/dynamic-programming/dsa05034/dsa05034.cpp
+++ submit/algo-design/dynamic-programming/dsa05034/dsa05034.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 using namespace std;
+const int MOD = 1e9 + 7;
 void testCase();
 int main() {
     // Write your code here
     int t;
-    cin  >> t;
-    while (t--) {
+    cin >> t;
+    while(t--) {
         testCase();
         cout << endl;
     }
     return 0;
 }
 void testCase() {
-    string s;
-    cin >> s;
-    int n = s.size();
+    int n, k;
+    cin >> n >> k;
     int dp[n + 1] = {0};
     dp[0] = 1;
     for (int i = 1; i <= n; i++) {
-        if (s[i - 1] != '0') {
-            dp[i] += dp[i - 1];
+        for (int j = 1; j <= k; j++) {
+            if (i - j >= 0) {
+                dp[i] = (dp[i] + dp[i - j]) % MOD;  
+            }
         }
-        if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
-            dp[i] += dp[i - 2];
-        }
-    }
+    } 
     cout << dp[n];
 }
