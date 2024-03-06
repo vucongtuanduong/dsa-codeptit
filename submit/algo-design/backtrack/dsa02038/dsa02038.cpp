a857ffa496c552e7edce4e13529599bac1f6d4a0
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-03-06 11:10:22 +0700

    [BACKTRACKING]

--- submit/algo-design/backtrack/dsa02038/dsa02038.cpp
+++ submit/algo-design/backtrack/dsa02038/dsa02038.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+vector<vector<int>> res;
 void testCase();
+void findSubset(vector<int>a, vector<int> &subset, int start, int k);
 int main() {
     // Write your code here
     int t;
@@ -11,8 +13,33 @@ int main() {
 
     return 0;
 }
+void findSubset(vector<int>a, vector<int> &subset, int start, int k) {
+    if (subset.size() == k) {
+        res.push_back(subset);
+    }
+    for (int i = start; i < a.size(); i++) {
+        subset.push_back(a[i]);
+        findSubset(a, subset, i + 1, k);
+        subset.pop_back();
+    }
+}
 void testCase() {
-    int n;
-    cin >> n;
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int> subset;
+    res.clear();
+    sort(a.begin(), a.end());
+    findSubset(a, subset, 0, k);
+    sort(res.begin(), res.end());
+    for (auto x : res) {
+        for (int i = 0; i < x.size(); i++) {
+            cout << x[i] << " ";
+        } 
+        cout << endl;
+    }
+
 }
