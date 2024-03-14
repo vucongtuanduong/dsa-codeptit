65461ec6482dd062deb37311975bff659702baae
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-03-14 10:12:46 +0700

    [GRAPH]

--- submit/algo-design/graph/dsa09016/dsa09016.cpp
+++ submit/algo-design/graph/dsa09016/dsa09016.cpp
@@ -1,11 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+struct Graph
+{   
+    int nV;
+    int nE;
+    vector<int> edges[1001];
+};
+void testCase();
+bool hasCycle(Graph *g);
+bool findCycle(Graph *g, int u, bool *visited, bool *recStack);
 int main() {
     // Write your code here
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
+}
+bool findCycle(Graph *g, int u, bool *visited, bool *recStack) {
+    if (visited[u] == false) {
+        visited[u] = true;
+        recStack[u] = true;
+        for (int i =0; i < g->edges[u].size(); i++) {
+            int current = g->edges[u][i];
+            if (!visited[current] && findCycle(g, current, visited, recStack)) {
+                return true;
+            } else if (recStack[current]) {
+                return true;
+            }
+        }
+    }
+    recStack[u] = false;
+    return false;
+}
+bool hasCycle(Graph *g) {
+    bool *visited = new bool[g->nV];
+    bool *recStack = new bool[g->nV];
+    for (int i = 0; i < g->nV; i++) {
+        visited[i] = false;
+        recStack[i] = false;
+    }
+    for (int i = 0; i < g->nV; i++) {
+        if (findCycle(g, i, visited, recStack)) {
+            return true;
+        }
+    }
+    return false;
+}
+void testCase() {
+    int nV, nE;
+    cin >> nV >>  nE;
+    Graph *g = new Graph;
+    g->nV = nV;
+    g->nE = nE;
+    for (int i = 0; i < g->nE; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        g->edges[u].push_back(v);
+    }
+    if (hasCycle(g)) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
 }
--- submit/algo-design/graph/dsa09019/dsa09019.cpp
+++ submit/algo-design/graph/dsa09019/dsa09019.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stack>
+#include <vector>
+#include <set>
 using namespace std;
 struct Graph{
     int nV;
@@ -6,10 +9,14 @@ struct Graph{
     vector<int> edges[1001];
 };
 void testCase();
-void dfs(Graph *g, int v, bool *visited);
-bool hasPath();
+bool hasCycle(Graph *g);
+bool findCycle(Graph *g, int u, bool *visited, int parent);
 int main() {
     // Write your code here
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     int t;
     cin >> t;
     while (t--) {
@@ -18,8 +25,33 @@ int main() {
     }
     return 0;
 }
-void dfs(Graph *g, int v, bool *visited) {
-
+bool findCycle(Graph *g, int u, bool *visited, int parent) {
+    visited[u] = true;
+    for (int i = 0; i < g->edges[u].size(); i++) {
+        int current = g->edges[u][i];
+        if (!visited[current]) {
+            if (findCycle(g, current, visited, u)) {
+                return true;
+            } 
+        } else if (current != parent) {
+            return true;
+        }
+    }
+    return false;
+}
+bool hasCycle(Graph *g) {
+    bool *visited = new bool [g->nV];
+    for (int i = 0; i < g->nV; i++ ){
+        visited[i] = false;
+    }
+    for (int i = 0; i < g->nV; i++) {
+        if (!visited[i]) {
+            if (findCycle(g, i, visited, -1)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 void testCase() {
     int nV, nE;
@@ -30,7 +62,14 @@ void testCase() {
     for (int i = 0; i < nE; i++) {
         int u, v;
         cin >> u >> v;
+        u--;
+        v--;
         g->edges[u].push_back(v);
         g->edges[v].push_back(u);
+    }
+    if (hasCycle(g)) {
+        cout << "YES";
+    } else {
+        cout << "NO";
     }
 }
