08aca7d98a872dafe5ac03bd9ecc68adc4ba777e
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-20 12:28:48 +0000

    revise 1/3 generative

--- submit/algo-design/generative/dsa01006/dsa01006_1.cpp
+++ submit/algo-design/generative/dsa01006/dsa01006_1.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> a(n + 1);
-vector<bool> used(n + 1, false);
+vector<int> a;
+vector<bool> used;
 vector<vector<int>> res;
 void testCase();
 void Try(int i);
@@ -16,13 +16,31 @@ int main() {
     return 0;
 }
 void Try(int i) {
-    for (int j = 1; j < a.size(); j++) {
-        
+    for (int j = 1; j <= a.size(); j++) {
+        if (used[j] == false) {
+            used[j] = true;
+            a[i] = j;
+            if (i == a.size() - 1) {
+                res.push_back(a);
+            } else {
+                Try(i + 1);
+            }
+            used[j] = false;
+        }
     }
 }
 void testCase() {
+    a.clear();
+    res.clear();
     int n;
     cin >> n;
-    Try(1);
-    
+    a.resize(n);
+    used.resize(n, false);
+    Try(0);
+    for (int i = res.size() - 1; i >= 0; i--) {
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] ;
+        }
+        cout << " ";
+    }
 }
