c50385595318e3700ddad0c8962df3c1f538be9d
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-20 12:11:07 +0000

    rename md file

---
+++ submit/algo-design/generative/dsa01006/dsa01006_1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> a(n + 1);
+vector<bool> used(n + 1, false);
+vector<vector<int>> res;
+void testCase();
+void Try(int i);
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
+void Try(int i) {
+    for (int j = 1; j < a.size(); j++) {
+        
+    }
+}
+void testCase() {
+    int n;
+    cin >> n;
+    Try(1);
+    
+}
