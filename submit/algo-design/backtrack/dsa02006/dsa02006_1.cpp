9cabfc9fc4268ac5dd763771fd0652568ee5a165
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-20 15:31:44 +0000

    revise backtrack 2/3

--- submit/algo-design/backtrack/dsa02006/dsa02006_1.cpp
+++ submit/algo-design/backtrack/dsa02006/dsa02006_1.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, k;
+vector<int> a, b;
 void testCase();
+void Try(int sum);
 int main() {
     // Write your code here
     int t;
@@ -11,6 +14,14 @@ int main() {
     }
     return 0;
 }
-void testCase() {
+void Try(int sum) {
     
+}
+void testCase() {
+    cin >> n >> k;
+    a.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    Try(k);
 }
