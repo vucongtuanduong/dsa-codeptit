39fc091a265a548dac0d837a17fc365332aa1cbb
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-20 15:52:46 +0000

    revise backtracking

--- submit/algo-design/backtrack/dsa02006/dsa02006_1.cpp
+++ submit/algo-design/backtrack/dsa02006/dsa02006_1.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int n, k;
 vector<int> a, b;
+bool ok = false;
 void testCase();
-void Try(int sum);
+void Try(int sum, int start);
 int main() {
     // Write your code here
     int t;
@@ -14,14 +15,37 @@ int main() {
     }
     return 0;
 }
-void Try(int sum) {
-    
+void Try(int sum, int start) {
+    if (sum == 0) {
+        ok = true;
+        cout << "[";
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i];
+            if (i != b.size() - 1) {
+                cout << " ";
+            }
+        }
+        cout << "] ";
+    }
+    for (int i = start; i < n; i++) {
+        if (a[i] <= sum) {
+            b.push_back(a[i]);
+            Try(sum - a[i], i + 1);
+            b.pop_back();
+        }
+    }
 }
 void testCase() {
     cin >> n >> k;
     a.resize(n);
+    b.clear();
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    Try(k);
+    sort(a.begin(), a.end());
+    Try(k, 0);
+    if (!ok) {
+        cout << "-1";
+    }
+    ok = false;
 }
