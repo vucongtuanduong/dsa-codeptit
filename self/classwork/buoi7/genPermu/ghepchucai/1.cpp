b78b3187fabea549e1864032b5ae273b8386ed24
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-22 08:06:52 +0000

    edit form

--- self/classwork/buoi7/genPermu/ghepchucai/1.cpp
+++ self/classwork/buoi7/genPermu/ghepchucai/1.cpp
@@ -2,69 +2,57 @@
 using namespace std;
 int a[101],  n;
 char c;
+bool ok = true;
 void init();
 void display();
 void genNext();
-bool isFinal();
 bool check(int x);
-bool check1();
 int main () {
     init();
-    while (!isFinal()) {
+    while (ok) {
         display();
         genNext();
     }
-    display();
     return 0;
 }
-bool check1() {
-    for (int i = 1; i <= n; i++) {
-        if (a[i] == 1 || a[i] == 5) {
-            if (i > 1 && i < n && check(a[i -1]) && check(a[i + 1])) {
-                return false;
-            }
-        }
-        
-        
-    }
-    return true;
-}
+
 bool check(int x) {
     return (x != 1) && (x != 5);
 }
 void genNext() {
     int j = n - 1;
-    while (a[j] > a[j + 1]) {
+    while (j > 0 && a[j] > a[j + 1]) {
         j--;
     }
-    int k = n;
-    while (a[k] < a[j]) {
-        k--;
-    }
-    swap(a[j], a[k]);
-    int l  = j + 1, r = n;
-    while (l <= r) {
-        swap(a[l], a[r]);
-        l++;
-        r--;
-    }
-}
-bool isFinal() {
-    for (int i = 1; i <= n - 1; i++) {
-        if (a[i] < a[i + 1]) {
-            return false;
+    if (j > 0) {
+        int k = n;
+        while (a[k] < a[j]) {
+            k--;
         }
+        swap(a[j], a[k]);
+        int l  = j + 1, r = n;
+        while (l <= r) {
+            swap(a[l], a[r]);
+            l++;
+            r--;
+        }
+    } else {
+        ok = false;
     }
-    return true;
+    
 }
+
 void display() {
     
-    if (check1()) {
-        for (int i = 1; i <= n; i++) {
-            cout << (char)(a[i] + 'A' - 1) ;
+    for (int i= 2; i < n; i++) {
+        if (!check(a[i]) && check(a[i - 1]) && check(a[i + 1])) {
+            return;
         }
-        cout << endl;
     }
+    for (int i = 1; i <= n; i++) {
+        cout << (char)(a[i] + 'A' - 1) ;
+    }
+    cout << endl;
     
 }
 void init() {
--- self/classwork/buoi7/genPermu/template/genPermu.cpp
+++ self/classwork/buoi7/genPermu/template/genPermu.cpp
@@ -1,49 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 int a[101],  n;
+
+bool ok = true;
 void init();
 void display();
 void genNext();
-bool isFinal();
 int main () {
     init();
-    while (!isFinal()) {
+    while (ok) {
         display();
         genNext();
     }
-    display();
     return 0;
 }
+
 void genNext() {
     int j = n - 1;
-    while (a[j] > a[j + 1]) {
+    while (j > 0 && a[j] > a[j + 1]) {
         j--;
     }
-    int k = n;
-    while (a[k] < a[j]) {
-        k--;
-    }
-    swap(a[j], a[k]);
-    int l  = j + 1, r = n;
-    while (l <= r) {
-        swap(a[l], a[r]);
-        l++;
-        r--;
-    }
-}
-bool isFinal() {
-    for (int i = 1; i <= n - 1; i++) {
-        if (a[i] < a[i + 1]) {
-            return false;
+    if (j > 0) {
+        int k = n;
+        while (a[k] < a[j]) {
+            k--;
         }
+        swap(a[j], a[k]);
+        int l  = j + 1, r = n;
+        while (l <= r) {
+            swap(a[l], a[r]);
+            l++;
+            r--;
+        }
+    } else {
+        ok = false;
     }
-    return true;
+    
 }
+
 void display() {
     for (int i = 1; i <= n; i++) {
         cout << a[i] << " ";
     }
     cout << endl;
+    
 }
 void init() {
     cin >> n;
