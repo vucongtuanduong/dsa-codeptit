64a8cbb02ddfe7bb49b698f50669d1f3b4a69a28
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-22 07:16:08 +0000

    dsa01021

--- self/classwork/buoi7/genCombination/nextComb/1.cpp
+++ self/classwork/buoi7/genCombination/nextComb/1.cpp
@@ -1,40 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, a[101], k;
-int x
+set<int> se;
+int res;
 void init();
 bool isFinal();
 void result();
 void genNext();
+void testCase();
 int main () {
-    init();
-    while (!isFinal()) {
-        result();
-        genNext();
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
     }
-    result();
+    
     return 0;
 }
+void testCase() {
+    init();
+    if (isFinal()) {
+        cout << k;
+        return;
+    }
+    genNext();
+    result();
+}
 void init() {
+    res = 0;
+    se.clear();
     cin >> n >> k;
     for (int i = 1; i <= k; i++) {
-        a[i] = i;
+        cin >> a[i];
+        se.insert(a[i]);
     }
     
 }
 bool isFinal() {
     for (int i = 1; i <= k; i++) {
         if (a[i] != (n - k + i)) {
-            return false;
+            return  false;
         }
     }
     return true;
 }
 void result() {
     for (int i = 1; i <= k; i++) {
-        cout << a[i] << " ";
+        if (se.count(a[i]) != 1) {
+            res++;
+        }
     }
-    cout << endl;
+    cout << res;
 }
 void genNext() {
     int t = k;
@@ -45,4 +62,5 @@ void genNext() {
     for (int i = t + 1; i <= k; i++) {
         a[i] = a[i - 1] + 1;
     }
+    
 }
