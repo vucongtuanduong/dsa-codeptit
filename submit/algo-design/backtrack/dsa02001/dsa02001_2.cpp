0a1c307421e76480958c147315e1eb0224ea1671
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-22 08:38:39 +0000

    dsa02001

---
+++ submit/algo-design/backtrack/dsa02001/dsa02001_2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> a;
+int n;
+void testCase();
+void Row();
+void init();
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        // cout << endl;
+    }
+    return 0;
+}
+void init() {
+    cin >> n;
+    a.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
+void Row() {
+    if (n == 0) {
+        return ;
+    } else {
+        cout << "[";
+        for (int i = 0; i < n; i++) {
+            cout << a[i];
+            if (i != n - 1) {
+                cout << " ";
+            }
+        }
+        cout << "]\n";
+        for (int i = 0; i < n - 1; i++) {
+            a[i] = a[i] + a[i + 1];
+        }
+        n--;
+        Row();
+
+    }
+
+}
+void testCase() {
+    init();
+    Row();
+} 
