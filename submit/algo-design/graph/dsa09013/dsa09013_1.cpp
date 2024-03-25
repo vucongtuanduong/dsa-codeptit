83ac69275ef38778335c5951f5eeb8cd64f415f3
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-25 12:20:13 +0000

    revise graph 1/3

--- submit/algo-design/graph/dsa09013/dsa09013_1.cpp
+++ submit/algo-design/graph/dsa09013/dsa09013_1.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-void testCase();
+
 struct Graph{
     int nV;
     int nE;
-    vector<int> edges[1001];
+    vector<vector<bool>> edges;
+    vector<pair<int,int>> listEdges;
 };
+
+bool dfs(Graph *g, int start);
+void listBridge(Graph *g);
+void testCase();
+
 int main() {
-    // Write your code here
     int t;
     cin >> t;
     while (t--) {
@@ -16,10 +21,55 @@ int main() {
     }
     return 0;
 }
+
+bool dfs(Graph *g, int start) {
+    vector<bool> visited(g->nV , false);
+    visited[start] = true;
+    stack<int> st;
+    st.push(start);
+    vector<int> path;
+    path.push_back(start);
+    while (!st.empty()) {
+        int u = st.top();
+        st.pop();
+        for (int i = 0; i < g->nV; i++ ) {
+            if (g->edges[u][i] == 1 && !visited[i]) {
+                st.push(u);
+                st.push(i);
+                visited[i] = true;
+                path.push_back(i);
+                break;
+            }
+        }
+    }
+    return path.size() == g->nV;
+}
+
+void listBridge(Graph *g) {
+    for (int i = 0; i < g->listEdges.size(); i++) {
+        g->edges[g->listEdges[i].first][g->listEdges[i].second] = 0;
+        g->edges[g->listEdges[i].second][g->listEdges[i].first] = 0;
+        if (!dfs(g, 0)) {
+            cout << g->listEdges[i].first + 1 << " " << g->listEdges[i].second + 1 << " ";
+        }
+        g->edges[g->listEdges[i].first][g->listEdges[i].second] = 1;
+        g->edges[g->listEdges[i].second][g->listEdges[i].first] = 1;
+    }
+}
+
 void testCase() {
     int nV, nE;
     cin >> nV >> nE;
     Graph *g = new Graph;
     g->nV = nV;
     g->nE = nE;
+    g->edges = vector<vector<bool>>(nV, vector<bool>(nV, 0));
+    for (int i = 0; i < g->nE; i++) {
+        int u, v;
+        cin >> u >> v;
+        g->edges[u - 1][v - 1] = 1;
+        g->edges[v - 1][u - 1] = 1;
+        g->listEdges.push_back(make_pair(u - 1, v - 1));
+    }
+    listBridge(g);
 }
