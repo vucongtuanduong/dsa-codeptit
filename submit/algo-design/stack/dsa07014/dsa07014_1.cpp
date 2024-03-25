b7e32f3fb9af7b0185b23ed11c344519cdd0dd7c
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-25 03:24:55 +0000

    revise stack

---
+++ submit/algo-design/stack/dsa07014/dsa07014_1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase();
+bool isOperator(char c);
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
+bool isOperator(char c) {
+    switch(c) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+    }
+    return false;
+}
+void testCase() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--) {
+        if (isOperator(s[i])) {
+            int op1 = st.top();st.pop();
+            int op2 = st.top();st.pop();
+            int res;
+            switch(s[i]) {
+                case '+':
+                    res = op1 + op2;
+                    st.push(res);
+                    break;
+                case '-':
+                    res = op1 - op2;
+                    st.push(res);
+                    break;
+                case '*':
+                    res = op1 * op2;
+                    st.push(res);
+                    break;
+                case '/':
+                    res = op1 / op2;
+                    st.push(res);
+                    break;
+
+            }
+        } else {
+            st.push(s[i] - '0');
+        }
+    }
+    cout << st.top();
+}
