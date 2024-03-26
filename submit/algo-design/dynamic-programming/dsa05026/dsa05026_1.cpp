ed813bd94c507e7f9e65aa7d6191579cd9fb1450
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-03-26 03:54:58 +0000

    revise dynamic programming 3/3

---
+++ submit/algo-design/dynamic-programming/dsa05026/dsa05026_1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase();
+int main() {
+    // Write your code here
+    testCase();
+    return 0;
+}
+void testCase() {
+    int c, n;
+    cin >> c >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int> dp(c + 1, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = c; j >= a[i]; j--) {
+            dp[j] = max(dp[j], dp[j - a[i]] + a[i]);
+        }
+    }
+    cout << dp[c];
+}
