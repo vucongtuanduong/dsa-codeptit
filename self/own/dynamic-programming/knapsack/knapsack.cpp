66eb1f2ff1977c6fa1f083bac5dae4333c69847a
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-02 15:43:22 +0000

    add knapsack

--- self/own/dynamic-programming/knapsack/knapsack.cpp
+++ self/own/dynamic-programming/knapsack/knapsack.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 int maxValue(vector<pair<int,int>> a, int weight) {
     int n = a.size();
-    vector<int> dp(n + 1, 0);
-    
+    vector<vector<int>>dp(n + 1, vector<int>(weight + 1, 0));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= weight; j++) {
+            int inc = 0, exc = 0;//include or exclude
+            if (a[i - 1].first <= j) {
+                inc =  a[i - 1].second + dp[i - 1][j - a[i - 1].first];
+            }
+            exc = dp[i - 1][j];
+
+            dp[i][j] = max(inc, exc);
+        }
+    }
+    return dp[n][weight];
 }
 int main() {
     // Write your code here
@@ -20,7 +31,7 @@ int main() {
         for (int i = 0; i < n; i++) {
             cin >> a[i].second;//value
         }     
-        cout << maxValue(a, w);
+        cout << maxValue(a, w) << endl;
     }
     return 0;
 }
