9569150c4334d50f4070c99083c2dc437647d912
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-07 05:02:16 +0000

    edit

--- self/own/practice/1/maso/maso.cpp
+++ self/own/practice/1/maso/maso.cpp
@@ -7,13 +7,23 @@ int x[101];
 bool unused[101];
 void testCase();
 void init();
+void TryChar(int i);
+void TryNum(int i);
 int main() {
     // Write your code here
     testCase();
     return 0;
 }
+void TryChar(int i) {
+    for (int j = 1; j <= n; j++)  {
+        
+    }
+}
 void init() {
     cin >> n;
+    for (int i = 1; i <= n; i++) {
+        unused[i] = true;
+    }
 }
 void testCase() {
     init();
