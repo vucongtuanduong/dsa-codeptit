379757cdd09dbb23d22907d2beb09d47ccc6ac60
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-07 04:59:52 +0000

    practice permutation

---
+++ self/own/practice/1/maso/maso.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string res;
+int a[101];
+int x[101];
+bool unused[101];
+void testCase();
+void init();
+int main() {
+    // Write your code here
+    testCase();
+    return 0;
+}
+void init() {
+    cin >> n;
+}
+void testCase() {
+    init();
+    Try(a, 1);
+    Try(x, 1);
+
+}
