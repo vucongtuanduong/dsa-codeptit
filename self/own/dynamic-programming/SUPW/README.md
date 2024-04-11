299d4e65e1e71689c7891770dbf4acd13c9bd95f
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-11 15:40:22 +0000

    edit file

--- self/own/dynamic-programming/SUPW/README.md
+++ self/own/dynamic-programming/SUPW/README.md
@@ -6,6 +6,7 @@
 
 ### 1
 5 
-2 2 3 2 2
-answer: 3
-f(i): 2 
+2 2 3 2 2\
+answer: 3\
+f(i): 2 2 3 4 4
+the answer will be the minimum in 3 last values because i can stop at every day in the last 3 days
--- self/own/dynamic-programming/SUPW/SUPW.cpp
+++ self/own/dynamic-programming/SUPW/SUPW.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main() {
     // Write your code here
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int dp[n] = {0};
+    for (int i = 0; i < n; i++) {
+        
+    }
     return 0;
 }
