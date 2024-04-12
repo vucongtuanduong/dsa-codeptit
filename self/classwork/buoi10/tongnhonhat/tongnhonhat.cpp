572a06c4b523d51cc1e0685803bca1dfbcfe7f48
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-12 06:59:26 +0000

    tongnhonhat

---
+++ self/classwork/buoi10/tongnhonhat/tongnhonhat.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase();
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
+void testCase() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    string x = "";
+    string y = "";
+    int i = 0;
+    while (i < n) {
+        x += to_string(a[i]);
+        i++;
+    }
+} 
