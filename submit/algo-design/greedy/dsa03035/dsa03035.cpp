955a627274fcb4c3813dce026ce3a284b844159a
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-14 14:50:19 +0000

    edit file

--- submit/algo-design/greedy/dsa03035/dsa03035.cpp
+++ submit/algo-design/greedy/dsa03035/dsa03035.cpp
@@ -16,8 +16,11 @@ void testCase() {
     vector<int> dp(n + 1, 0);
     int leftMax = 0;
     for (int i = 0; i < n; i++) {
-        dp[a[i]] = dp[a[i]  - 1] + 1;
-        cout << "dp[a[i]]: " << dp[a[i]] << ", dp[a[i] - 1]: " << dp[a[i] - 1] << endl;
+        // cout << "a[i]: " << a[i] << ", a[i] - 1: " << a[i] - 1 << endl;
+        
+        dp[a[i]] = dp[a[i]  - 1] + 1;    
+        // cout << "dp[a[i]]: " << dp[a[i]] << ", dp[a[i] - 1]: " << dp[a[i] - 1] << endl;
+        // cout << "-------------------------\n";
         leftMax = max(leftMax, dp[a[i]]);
     }
     cout << n - leftMax;
