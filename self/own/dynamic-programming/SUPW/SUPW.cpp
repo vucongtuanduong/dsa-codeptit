109c0f6f043aeb28cf4f35ae333bb05f88df528b
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-15 03:19:29 +0000

    IPL implementation

--- self/own/dynamic-programming/SUPW/SUPW.cpp
+++ self/own/dynamic-programming/SUPW/SUPW.cpp
@@ -10,8 +10,16 @@ int main() {
         cin >> a[i];
     }
     int dp[n] = {0};
-    for (int i = 0; i < n; i++) {
-        
+    dp[0] = a[0];
+    dp[1] = a[1];
+    dp[2] = a[2];
+    for (int i = 3; i < n; i++) {
+        dp[i] = min({dp[i - 1], dp[i - 2], dp[i - 3]}) + a[i];
     }
+    for (int i = 0; i < n; i++) {
+        cout << dp[i] << " ";
+    }
+    cout << endl;
+    cout << min({dp[n - 1], dp[n - 2], dp[n - 3]}) << endl;
     return 0;
 }
