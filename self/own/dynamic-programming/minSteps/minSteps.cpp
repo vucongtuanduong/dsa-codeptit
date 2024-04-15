3e28f6c44f282a1c8fdd0e615b4bc91ac9fa2ba0
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-15 03:38:07 +0000

    edit

--- self/own/dynamic-programming/minSteps/minSteps.cpp
+++ self/own/dynamic-programming/minSteps/minSteps.cpp
@@ -1,7 +1,16 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     // Write your code here
+    int n;
+    cin >> n;
+    int ans = INT_MAX;
+    if (n % 2 == 0) {
+        ans = min(ans, n / 2);
+
+    }
+    if (n % 3)
+    vector<int> dp(n, 0);
     return 0;
 }
