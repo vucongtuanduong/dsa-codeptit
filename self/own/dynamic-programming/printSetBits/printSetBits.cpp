05872bceb1e1aea1c1d73dc3554bd4fb3995800a
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-15 15:20:22 +0000

    printSetBits

--- self/own/dynamic-programming/printSetBits/printSetBits.cpp
+++ self/own/dynamic-programming/printSetBits/printSetBits.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main() {
     // Write your code here
+    int n = 10;
+    int dp[n + 1];
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = dp[i / 2] + (i % 2);
+        cout << dp[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
