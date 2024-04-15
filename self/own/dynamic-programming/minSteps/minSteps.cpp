354b193a73e6bc003b9dd6c3c3c786c85f15459e
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-15 03:41:01 +0000

    finalize minSteps

--- self/own/dynamic-programming/minSteps/minSteps.cpp
+++ self/own/dynamic-programming/minSteps/minSteps.cpp
@@ -1,16 +1,30 @@
  #include <bits/stdc++.h>
 using namespace std;
-
+int memo[100];
+int dp(int n) {
+    if (n == 1) {
+        return 0;
+    }
+    int &ans = memo[n];
+    if (ans != -1) {
+        return ans;
+    }
+    ans = INT_MAX;
+    if (n % 2 == 0) {
+        ans = min(ans, dp(n / 2));
+    }
+    if (n % 3 == 0) {
+        ans = min(ans, dp(n / 3));
+    }
+    ans = min(ans, dp(n - 1));
+    ans ++;
+    return ans;
+}
 int main() {
     // Write your code here
     int n;
     cin >> n;
-    int ans = INT_MAX;
-    if (n % 2 == 0) {
-        ans = min(ans, n / 2);
-
-    }
-    if (n % 3)
-    vector<int> dp(n, 0);
+    memset(memo, -1, sizeof(memo));
+    cout << dp(n);
     return 0;
 }
