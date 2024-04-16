4670eda51cfc173bfa52e78770c5ca71b069471e
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-16 04:07:17 +0000

    redo dsa05029 after learning videos

--- submit/algo-design/dynamic-programming/dsa05029/dsa05029_2.cpp
+++ submit/algo-design/dynamic-programming/dsa05029/dsa05029_2.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+string s;
+int n;
+int dp(int index) {
+    if (index == n) {
+        return 1;
+    }
+    int res = 0;
+    if (s[index] >= '1' && s[index] <= '9') {
+        res += dp(index + 1);
+    }
+    if (index + 1 < n && (s[index] == '1')) {
+        res += dp(index + 2);
+    }
+    if (index + 1 < n && (s[index] == '2' && s[index + 1] <= '6')) {
+        res += dp(index + 2);
+    }
+    return res;
+}
+void testCase() {
+    
+    cin >> s;
+    n = s.size();
+    cout << dp(0);
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
