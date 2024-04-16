54c5c0b9743ece7e507f12f19bad548a8b1c4a9c
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-16 08:31:03 +0000

    palindromePartition

--- self/own/dynamic-programming/palindromePartition/palindromePartition.cpp
+++ self/own/dynamic-programming/palindromePartition/palindromePartition.cpp
@@ -3,6 +3,7 @@ using namespace std;
 class Solution {
 public:
     string s;
+    map<int , vector<vector<string>>> mp;
     bool isPalindrome(int i, int j) {
         while (i <= j) {
             if (s[i] != s[j]) {
@@ -14,14 +15,30 @@ public:
         return true;
     }
     vector<vector<string>> dp(int i) {
-        vector<vector<string>> res;
+        if (i == s.size()) {
+            return vector<vector<string>>();
+        }
+        if (mp.count(i)) {
+            return mp[i];
+        }
+        vector<vector<string>> ans;
         for (int j = i; j < s.size(); j++) {
             vector<string> temp;
             if (isPalindrome(i, j)) {
-                temp.push_back(s.substr(i, j));
-                res.push_back(temp);
+                vector<vector<string>> res = dp(j + 1);
+                string substring = s.substr(i, j - i + 1);
+                for (auto &x : res) {
+                    x.insert(x.begin(), substring);
+                }
+                if (res.empty()) {
+                    res.push_back({substring});
+                }
+                for (auto x : res) {
+                    ans.push_back(x);
+                }
             }
         }
+        return mp[i] = ans;
     }
     vector<vector<string>> partition(string s) {
         this->s = s;
