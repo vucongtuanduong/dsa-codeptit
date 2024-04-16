88a506a1c21bbd09c7ac17fcaa22786b19204f92
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-16 05:02:54 +0000

    edit dsa05029 memoisastion

--- submit/algo-design/dynamic-programming/dsa05029/dsa05029_2.cpp
+++ submit/algo-design/dynamic-programming/dsa05029/dsa05029_2.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 string s;
 int n;
+vector<int> memo;
 int dp(int index) {
     if (index == n) {
         return 1;
+    }
+    if (memo[index] != -1) {
+        return memo[index];
     }
     int res = 0;
     if (s[index] >= '1' && s[index] <= '9') {
@@ -16,12 +20,15 @@ int dp(int index) {
     if (index + 1 < n && (s[index] == '2' && s[index + 1] <= '6')) {
         res += dp(index + 2);
     }
+    memo[index] = res;
     return res;
 }
 void testCase() {
     
     cin >> s;
     n = s.size();
+    memo.clear();
+    memo.resize(n, -1);
     cout << dp(0);
 }
 int main() {
