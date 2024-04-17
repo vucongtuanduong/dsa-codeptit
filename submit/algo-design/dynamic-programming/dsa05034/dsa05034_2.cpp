c11a15834605089c2e43e61e02cb7befafaa3027
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-17 02:48:07 +0000

    dynamic programming

--- submit/algo-design/dynamic-programming/dsa05034/dsa05034_2.cpp
+++ submit/algo-design/dynamic-programming/dsa05034/dsa05034_2.cpp
@@ -15,4 +15,14 @@ int main() {
 void testCase() {
     int n, k;
     cin >> n >> k;
+    vector<long long> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (i - j >= 0) {
+                dp[i] = (dp[i] + dp[i - j]) % MOD;
+            }
+        }
+    }
+    cout << dp[n];
 }
