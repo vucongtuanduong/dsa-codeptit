335ae0f1645bc47d701b39372f7ad82a76ea707e
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-18 10:03:17 +0000

    2864 submmited

--- self/own/practice/1/2864/2864.cpp
+++ self/own/practice/1/2864/2864.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n = s.size();
+        int count1s = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '1') {
+                count1s++;
+            }
+        }
+        int count0s = n - count1s;
+        string res = "";
+        for (int i = 0; i < count1s - 1; i++) {
+            res += "1";
+        }
+        for (int i = 0; i < count0s; i++ ){
+            res += "0";
+        }
+        res += "1";
+        return res;
+    }
+};
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution h;
+        cout << h.maximumOddBinaryNumber(s) << endl;
+    }
     return 0;
 }
