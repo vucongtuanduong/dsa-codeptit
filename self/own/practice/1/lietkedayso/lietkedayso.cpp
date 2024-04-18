e6d548a6aed3c96d147a592d2719c7f5af0a3257
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-18 15:13:47 +0000

    lietkedayso

---
+++ self/own/practice/1/lietkedayso/lietkedayso.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int a[101];
+int dd[4] = {2, 3, 5, 7};
+void Try(int i) {
+    for (int j = 1; j <= i; j++) {
+
+    }
+}
+void testCase() {
+    cin >> n;
+    for (int i = 4; i <= n; i++ ) {
+        Try(1);
+    }
+
+}
+int main() {
+    // Write your code here
+    testCase();
+    return 0;
+}
