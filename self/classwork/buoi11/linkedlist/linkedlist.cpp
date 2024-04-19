b912f04d06404a697adfc43d5dd723218a1d2b8b
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-04-19 07:56:21 +0000

    edit linkedlist

--- self/classwork/buoi11/linkedlist/linkedlist.cpp
+++ self/classwork/buoi11/linkedlist/linkedlist.cpp
@@ -11,7 +11,19 @@ void insert(int new_data) {
     ptr->next = head;
     head = ptr;
 }
+void display() {
+    node *ptr = head;
+    while (ptr != NULL) {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
 int main() {
     // Write your code here
+    insert(3);
+    insert(1);
+    insert(9);
+    display();
     return 0;
 }
