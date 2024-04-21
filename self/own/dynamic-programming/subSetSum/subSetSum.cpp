dbeaf94458793c29034ba5a5c01c4131ad7f0f15
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-21 10:38:27 +0000

    subsetSum

--- self/own/dynamic-programming/subSetSum/subSetSum.cpp
+++ self/own/dynamic-programming/subSetSum/subSetSum.cpp
@@ -1,7 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N = 1000;
+int a[N], n;
+vector<vector<int>> memo;
+//whether sum is possible from subset of
+//a [.. index]
+int subsetSum(int index, int sum) {
+    if (index == -1 ) {
+        return (sum == 0);
+    }
+    if (memo[index][sum] != -1) {
+        return memo[index][sum];
+    }
+    bool ans = false;
+    //include a[index] in sum
+    if (sum >= a[index]) {
+        ans |= subsetSum(index - 1, sum - a[index]);
+    }
 
+    //exclude 
+    ans |= subsetSum(index - 1, sum);
+    return memo[index][sum] = ans;
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum;
+        cin >> sum >> n;
+        memo.resize(n, vector<int> (sum + 1, -1));
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << subsetSum(n - 1, sum) << endl;
+    }
+    
     return 0;
 }
