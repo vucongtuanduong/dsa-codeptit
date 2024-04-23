80e35004a693bda58b0429d5efb86238a0f5b9ac
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-23 09:21:24 +0000

    edit dsa10009

--- submit/algo-design/graph/dsa10009/README.md
+++ submit/algo-design/graph/dsa10009/README.md
@@ -1,1 +1,2 @@
 ## dsa10009
+![alt text](image.png)
--- submit/algo-design/graph/dsa10009/dsa10009.cpp
+++ submit/algo-design/graph/dsa10009/dsa10009.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 void testCase() {
-    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> edges;
+    edges.resize(n, vector<int>(n, INT_MAX));
+    for (int i = 0; i < n; i++) {
+        edges[i][i] = 0;
+    }
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges[u - 1][v - 1] = w;
+        edges[v - 1][u - 1] = w;
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (edges[i][k] < INT_MAX && edges[k][j] < INT_MAX) // Avoid overflow
+                    edges[i][j] = min(edges[i][j], edges[i][k] + edges[k][j]);
+            }
+        }
+    }
+    int q;
+    cin >> q;
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << edges[u - 1][v - 1] << endl;
+        
+    }
 }
 int main() {
     // Write your code here
