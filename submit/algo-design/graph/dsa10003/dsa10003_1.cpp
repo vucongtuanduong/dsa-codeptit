5b545696fb6028d32a073eadf499c7e3c42f4a73
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-23 03:53:36 +0000

    redo disjointset problem

--- submit/algo-design/graph/dsa10003/dsa10003_1.cpp
+++ submit/algo-design/graph/dsa10003/dsa10003_1.cpp
@@ -5,6 +5,39 @@ struct Graph{
     int nE;
     vector<pair<int, int>>edges;
 };
+class DisjointSet {
+    private:
+        vector<int>parent;
+    public:
+        DisjointSet(int n) {
+            parent.resize(n + 1, -1);
+        }
+        int find(int x) {
+            if (parent[x] == -1) {
+                return x;
+            }
+            return parent[x] = find(parent[x]);
+        }
+        void unionSets(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                parent[rootX] = rootY;
+            }
+        }
+};
+bool hasCycle(Graph *g) {
+    DisjointSet dsu(g->nV);
+    for (auto &edge : g->edges) {
+        int u = edge.first;
+        int v = edge.second;
+        if (dsu.find(u) == dsu.find(v)) {
+            return true;
+        }
+        dsu.unionSets(u, v);
+    }
+    return false;
+}
 void testCase() {
     int nV , nE;
     cin >> nV >> nE;
@@ -16,7 +49,11 @@ void testCase() {
         cin >> u >> v;
         g->edges.push_back(make_pair(u - 1, v- 1));
     }
-    cout << hasCycle(g->edges, g->nV);
+    if (hasCycle(g)) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
 }
 int main() {
     // Write your code here
---
+++ submit/algo-design/graph/dsa10015/dsa10015_1.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Edge{
+    int u, v, w;
+
+};
+struct Graph{
+    int nV;
+    int nE;
+    vector<Edge>edges;
+};
+class DisjointSet {
+    private:
+        vector<int>parent, rank;
+    public:
+        DisjointSet(int n) {
+            parent.resize(n + 1, -1);
+            rank.resize(n + 1, 0);
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+            }
+        }
+        int find(int x) {
+            if (x != parent[x]) {
+                return parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+        void unionSets(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] < rank[rootY]) {
+                    swap(rootX , rootY);
+                }
+                parent[rootY] = rootX;
+                if (rank[rootX] == rank[rootY]) {
+                    rank[rootX]++;
+                }
+            }
+        }
+};
+bool cmp(Edge a, Edge b) {
+    return a.w < b.w;
+}
+vector<Edge> kruskalMST(Graph *g) {
+    sort(g->edges.begin(), g->edges.end(), cmp);
+    DisjointSet dsu(g->nV);
+    vector<Edge> mst;
+    for (Edge edge : g->edges) {
+        int u = edge.u;
+        int v = edge.v;
+        int w = edge.w;
+        if (dsu.find(u) != dsu.find(v)) {
+            mst.push_back({u, v, w});
+            dsu.unionSets(u, v);
+        }
+    }
+    return mst;
+}
+void testCase() {
+    int nV , nE;
+    cin >> nV >> nE;
+    Graph *g = new Graph;
+    g->nV = nV;
+    g->nE = nE;
+    for (int i = 0; i < nE; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g->edges.push_back({u, v, w});
+    }
+    vector<Edge>mst = kruskalMST(g);
+    int mstCost = 0;
+    for (Edge edge : mst) {
+        mstCost += edge.w;
+
+    }
+    cout << mstCost;
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
