66849e2961e9fd17745a9245551414ececb48b3b
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-23 14:41:50 +0000

    demKituB

---
+++ self/own/practice/1/demKituB/demKituB.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
