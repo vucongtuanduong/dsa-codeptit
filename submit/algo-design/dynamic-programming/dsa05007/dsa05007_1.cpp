9b73f5b6282ac75a54bfe1e77fde25d8a4db291b
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-25 12:18:50 +0000

    dsa05007 redo dynamic programming

---
+++ submit/algo-design/dynamic-programming/dsa05007/dsa05007_1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    long long dp[n] =  {0};
+    dp[0] = a[0];
+    dp[1] = max(a[0], a[1]);
+    for(int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + a[i]);
+    }
+    // cout << *max_element(a, a + n);
+    cout << dp[n - 1];
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
