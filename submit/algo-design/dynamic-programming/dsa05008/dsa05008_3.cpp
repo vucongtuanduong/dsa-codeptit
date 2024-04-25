bab8b1f3dd8e6269c286fb21679237677131150c
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-25 14:31:25 +0000

    redo dynamic programming

--- submit/algo-design/dynamic-programming/dsa05008/dsa05008_3.cpp
+++ submit/algo-design/dynamic-programming/dsa05008/dsa05008_3.cpp
@@ -4,8 +4,23 @@ void testCase() {
     int n, s;
     cin >> n >> s;
     int a[n];
+    vector<bool> l(s + 1, false);
+    l[0] = true;
     for (int i = 0; i < n; i++){ 
         cin >> a[i];
+        for (int j = s; j >= a[i]; j--) {
+            if (l[j] == false &&  l[j - a[i]] == true) {
+                l[j] = 1;
+            }
+        }
+    }
+    // for (int i = 0; i < n; i++) {
+        
+    // }
+    if (l[s]) {
+        cout << "YES";
+    } else {
+        cout << "NO";
     }
 }
 int main() {
