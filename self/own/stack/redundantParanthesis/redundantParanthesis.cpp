163508c49b97533bbc4b967ebdcb543b72a12426
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-26 15:08:45 +0000

    redundant Paranthesis

--- self/own/stack/redundantParanthesis/redundantParanthesis.cpp
+++ self/own/stack/redundantParanthesis/redundantParanthesis.cpp
@@ -1,7 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+bool checkRedundant(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c != ')') {
+            st.push(c);
+        } else {
+            bool operator_found = false;
+            while (!st.empty() && st.top() != '(') {
+                char top = st.top();
+                if (top == '+' || top == '-' || top == '*' || top == '/') {
+                    operator_found = true;
+                }
+                st.pop();
+            }
+            st.pop();
+            if (!operator_found) {
+                return true;
+            }
+            
+        }
+        
+    }   
+    return false;
+}
+void testCase() {
+    string s;
+    cin >> s;
+    if (checkRedundant(s)) {
+        cout << "Contains redundant paranthesis";
+    } else {
+        cout << "Not contain redundant paranthesis";
+    }
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
