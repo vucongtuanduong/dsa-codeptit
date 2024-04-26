da866467d9072e21b8475e5a9463dfb32ad22ea2
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-26 07:22:45 +0000

    inFix-PostfFix final

--- self/classwork/buoi12/inFix-PostFix/inFix-PostFix.cpp
+++ self/classwork/buoi12/inFix-PostFix/inFix-PostFix.cpp
@@ -3,8 +3,12 @@ using namespace std;
 int countRank(char c) {
     if (c == '+' || c == '-') {
         return 1;
-    } 
-    return 2;
+    } else if (c == '*' || c == '/') {
+        return 2;
+    } else if (c == '^') {
+        return 3;
+    }
+    return -1;
 }
 string inFixtoPostFix(string s) {
     stack<char> st;
@@ -43,6 +47,7 @@ string inFixtoPostFix(string s) {
 void testCase() {
     string s;
     cin >> s;
+    // cout << s << endl;
     cout << inFixtoPostFix(s);
 }
 int main() {
