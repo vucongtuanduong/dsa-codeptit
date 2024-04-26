62499eed02c517374ad46466d5965c713d44407e
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-26 06:39:40 +0000

    edit

--- self/classwork/buoi12/ctdl005/ctdl005.cpp
+++ self/classwork/buoi12/ctdl005/ctdl005.cpp
@@ -5,6 +5,14 @@ struct Node {
     Node *next;
 };
 Node *head = NULL;
+void display() {
+    Node *current = head;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    
+}
 void insert(int new_data) { //insert at tail of the linked list
     Node *x = new Node;
     x->data = new_data;
@@ -20,26 +28,40 @@ void insert(int new_data) { //insert at tail of the linked list
     }
     
 }
-void deleteNode(int x) {
+int deleteNode(int x) {
+    int count = 0;
     Node *current = head;
+    Node *prev = NULL;
     while (current != NULL) {
+        Node *tmp = current;
         if (current->data == x) {
             current = current->next;
-            
+            if (prev != NULL) {
+                prev->next = current;
+            } else {
+                head = current;
+            }
+            delete tmp;
+            count++;
+        } else {
+            prev = current;
+            current = current->next;
         }
     }
+    return count;
 }
 void testCase() {
     int n;
     cin >> n;
     for (int i = 0; i < n; i++) {
         int value;
-        cin >>> value;
+        cin >> value;
         insert(value);
     }
     int x;
     cin >> x;
-    deleteNode(x);
+    int res = deleteNode(x);
+    display();
 }
 int main() {
     // Write your code here
