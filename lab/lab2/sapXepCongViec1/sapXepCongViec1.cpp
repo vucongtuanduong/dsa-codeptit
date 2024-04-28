0f9fec77fa4bd1d1c46156308c13112590aad951
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 02:07:54 +0000

    final sapxepcongviec

--- lab/lab2/sapXepCongViec1/sapXepCongViec1.cpp
+++ lab/lab2/sapXepCongViec1/sapXepCongViec1.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool cmp(pair<int,int> a, pair<int, int>  b) {
+    if (a.second == b.second) {
+        return a.first < b.first;
+    }
+    return a.second < b.second;
+}
 void testCase() {
-    
+    int n;
+    cin >> n;
+    vector<pair<int,int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].second;
+    }
+    sort(a.begin(), a.end(), cmp);
+    int res = 1;
+    int value = a[0].second;
+    for(int i = 1; i < n; i++) {
+        if (a[i].first >= value) {
+            res ++;
+            value = a[i].second;
+        } 
+    }
+    cout << res;
 }
 int main() {
     // Write your code here
