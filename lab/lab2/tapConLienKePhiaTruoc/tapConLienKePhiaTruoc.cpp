2052704d33b87160ae781cb34f69cfb4fdc4a51f
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 03:27:36 +0000

    giaimaxaukitu

--- lab/lab2/tapConLienKePhiaTruoc/tapConLienKePhiaTruoc.cpp
+++ lab/lab2/tapConLienKePhiaTruoc/tapConLienKePhiaTruoc.cpp
@@ -5,30 +5,7 @@ vector<int> a;
 vector<int> input;
 vector<int> precedence;
 bool ok = false;
-void check() {
-    if (input == a) {
-        ok = true;
-        for (int i = 1; i <= k; i++) {
-            cout << precedence[i] << " ";
-        }
-        return;
-    } else {
-        precedence = input;
-    }
-}
-void Try(int i) {
-    for (int j = input[i - 1] + 1; j <= (n - k + i); j++) {
-        input[i] = j;
-        if (ok == true) {
-            return;
-        }
-        if (i == k) {
-            check(); 
-        } else {
-            Try(i + 1);
-        }
-    }
-}
+
 void testCase() {
     cin >> n >> k;
     ok = false;
@@ -49,13 +26,15 @@ void testCase() {
         return;
     } else {
         int i = k;
-        while (i >= 1 && a[i - 1] == a[i] - 1) {
+        while (i >= 1 && a[i] == a[i - 1] + 1) {
             i--;
         }
         // cout << "i: " << i << endl;
-        a[i]--;
+        if (i != 0) {
+            a[i]--;
+        }
         for (int j = i + 1; j <= k; j++) {
-            a[j] = a[j - 1] + 1;
+            a[j] = n - k + j;
         }
         for (int j = 1; j <= k; j++) {
             cout << a[j] << " ";
