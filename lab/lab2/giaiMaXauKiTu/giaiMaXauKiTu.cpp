47473a14a4efe0b6d0849089ca3bbbcf6d0f9b66
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 01:48:29 +0000

    add 4

---
+++ lab/lab2/giaiMaXauKiTu/giaiMaXauKiTu.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    string s;
+    cin >> s;
+    
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
