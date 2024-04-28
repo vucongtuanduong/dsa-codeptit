27f21f101e540f09b7da67336a03c1b7ddb98353
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 01:05:55 +0000

    add 1

---
+++ lab/lab2/kiemTrabieuThucSoHoc/kiemTrabieuThucSoHoc.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(string s) {
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++){ 
+        
+    }
+}
+void testCase() {
+    string s;
+    cin >> s;
+    if (check(s)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
