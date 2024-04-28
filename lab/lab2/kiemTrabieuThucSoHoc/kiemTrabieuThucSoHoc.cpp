271a3dd250533ef02c0016c7ad60bc731b68ed02
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 01:15:36 +0000

    add file

--- lab/lab2/kiemTrabieuThucSoHoc/kiemTrabieuThucSoHoc.cpp
+++ lab/lab2/kiemTrabieuThucSoHoc/kiemTrabieuThucSoHoc.cpp
@@ -3,12 +3,36 @@ using namespace std;
 bool check(string s) {
     stack<char> st;
     for (int i = 0; i < s.size(); i++){ 
-        
+        bool operator_found = false;
+        if (s[i] != ')') {
+            st.push(s[i]);
+        } else {
+            while (st.top() != '(') {
+                char c = st.top();
+                // cout << "c : " << c << endl;
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    operator_found = true;
+                }
+                st.pop();
+            }
+            // cout << "st.top(): " << st.top() << " , operator_found: " << operator_found << endl;
+            st.pop();
+            if (!operator_found) {
+                return true;
+            }
+        }
     }
+    return false;
 }
 void testCase() {
     string s;
-    cin >> s;
+    getline(cin, s);
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            s.erase(s.begin() + i);
+        }
+    }
+    // cout << "s: " << s << endl;
     if (check(s)) {
         cout << "Yes";
     } else {
@@ -19,6 +43,7 @@ int main() {
     // Write your code here
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
         testCase();
         cout << endl;
