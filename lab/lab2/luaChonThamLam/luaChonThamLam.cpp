eda97b6c24b63a319206b09221f6c62e12384280
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 02:09:58 +0000

    add Luachonthamlam

---
+++ lab/lab2/luaChonThamLam/luaChonThamLam.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    int n, s;
+    cin >> n >> s;
+    string minn, maxx = "";
+}
+int main() {
+    // Write your code here
+    testCase();
+    return 0;
+}
