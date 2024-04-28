a844eddd0fc9326c329f1b7abb690cb8a5037297
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 02:03:28 +0000

    add noiDay

--- lab/lab2/dayConTangDan/dayConTangDan.cpp
+++ lab/lab2/dayConTangDan/dayConTangDan.cpp
@@ -3,14 +3,18 @@ using namespace std;
 int n;
 vector<int> a;
 vector<int> x;
+vector<string> res;
+string temp ;
 void result() {
     if (count(x.begin(), x.end(), 1) >= 2) {
         for (int i = 0; i < n; i++){ 
             if (x[i] == 1) {
-                cout << a[i] << " ";
+                temp += to_string(a[i]) + " ";
             }
         }
-        cout << endl;
+        temp.pop_back();
+        res.push_back(temp);
+        temp = "";
     }
     
 }
@@ -25,15 +29,19 @@ void Try(int i) {
     }
 }
 void testCase() {
-    
+    temp = "";
     cin >> n;
     a.resize(n);  
     x.resize(n);  
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    sort(a.begin(), a.end());
+    // sort(a.begin(), a.end());
     Try(0);
+    sort(res.begin(), res.end());
+    for (auto x : res) {
+        cout << x << endl;
+    }
 }
 int main() {
     // Write your code here
