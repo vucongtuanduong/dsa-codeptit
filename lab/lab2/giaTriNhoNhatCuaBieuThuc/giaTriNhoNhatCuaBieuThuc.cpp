bef1a6d2e02053f068f4af75fff52c01820d99d0
vctduong <165612382+vctduong@users.noreply.github.com>
2024-04-28 02:55:51 +0000

    add file

--- lab/lab2/giaTriNhoNhatCuaBieuThuc/giaTriNhoNhatCuaBieuThuc.cpp
+++ lab/lab2/giaTriNhoNhatCuaBieuThuc/giaTriNhoNhatCuaBieuThuc.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testCase() {
+    int n;
+    cin >> n;
+    int a[n];
+    int b[n];
+    for (int i =0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i =0 ; i < n; i++) {
+        cin >> b[i];
+    }
+    sort(a, a + n);
+    sort(b, b + n, greater<int>());
+    long long res= 0 ;
+    for (int i =0 ; i < n; i++) {
+        res += a[i] * b[i];
+    }
+    cout << res;
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
