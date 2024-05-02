e7cddf9f7dec863c681711a3dabd253552a4a482
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-02 03:48:06 +0000

    redo

--- submit/algo-design/divide-and-conquer/dsa04022/dsa04022_2.cpp
+++ submit/algo-design/divide-and-conquer/dsa04022/dsa04022_2.cpp
@@ -1,10 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+long long fibo[100];
+void init() {
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i < 100; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+}
+void testCase() {
+    long long n, k;
+    cin >> n >> k;
+    
+}
 int main() {
     // Write your code here
     int t;
     cin >> t;
+    init();
     while (t--) {
         testCase();
         cout << endl;
