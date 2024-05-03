7c25c01eb09a43f2bd6286b516c6aa6b1a7c9a15
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-03 06:46:43 +0000

    add file

--- self/classwork/buoi13/dsa08017/dsa08017.cpp
+++ self/classwork/buoi13/dsa08017/dsa08017.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testCase() {
+    queue<string> q;
+    stack<string> st;
+    int n;
+    cin >> n;
+    q.push("");
+    while (true) {
+        string s = q.front();
+        q.pop();
+        if (s.size() == n) {
+            continue;
+        }
+        st.push(s);
+        q.push(s + "6");
+        q.push(s + "8");
+    }
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
