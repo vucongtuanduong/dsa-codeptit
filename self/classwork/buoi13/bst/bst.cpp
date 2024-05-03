feccea9909a532499063695ba419241495bf655d
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-03 07:24:33 +0000

    edit tree

--- self/classwork/buoi13/bst/bst.cpp
+++ self/classwork/buoi13/bst/bst.cpp
@@ -5,7 +5,22 @@ struct Node {
     Node *left;
     Node *right;
 };
+Node *root = NULL;
+
+//tao node cho cay
+Node *makeNode (int data) {
+    Node *n = new Node();
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
 int main() {
     // Write your code here
+    root = makeNode(1);
+    Node *node2 = makeNode(2);
+    root->left = node2;
+    Node *node3 = makeNode(3);
+    root->right = node3;
     return 0;
 }
