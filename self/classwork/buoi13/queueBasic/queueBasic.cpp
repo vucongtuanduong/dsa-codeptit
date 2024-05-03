57c25d68b3cc72edd0dcc898d3693cc0156a13e7
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-03 06:32:11 +0000

    edit queue

--- self/classwork/buoi13/queueBasic/queueBasic.cpp
+++ self/classwork/buoi13/queueBasic/queueBasic.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main() {
     // Write your code here
     queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
     return 0;
 }
