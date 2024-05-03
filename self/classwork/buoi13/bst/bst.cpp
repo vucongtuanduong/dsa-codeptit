29ecc990b133b6cee58126bf3d805968b925271f
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-03 07:27:59 +0000

    edit

--- self/classwork/buoi13/bst/bst.cpp
+++ self/classwork/buoi13/bst/bst.cpp
@@ -22,5 +22,13 @@ int main() {
     root->left = node2;
     Node *node3 = makeNode(3);
     root->right = node3;
+    Node *node4 = makeNode(4);
+    Node *node5 = makeNode(5);
+    Node *node6 = makeNode(6);
+    Node *node7 = makeNode(7);
+    node2->left = node4;
+    node2->right = node5;
+    node5->left = node6;
+    node5->right = node7;
     return 0;
 }
