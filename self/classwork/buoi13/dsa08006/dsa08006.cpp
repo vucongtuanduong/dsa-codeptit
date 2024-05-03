288d9cb245f752a191406b2546be1a33a531cad6
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-03 06:38:13 +0000

    add dsa08006

--- self/classwork/buoi13/dsa08006/dsa08006.cpp
+++ self/classwork/buoi13/dsa08006/dsa08006.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testCase() {
+    int n;
+    cin >> n;
+    queue<string> q;
+    q.push("9");
+    while (true) {
+        string s = q.front();
+        long long temp = 0;
+        q.pop();
+        for (int i = 0; i < s.size(); i++) {
+            temp = (temp * 10 + (s[i] - '0')) % n;
+        }
+        if (temp == 0) {
+            cout << s;
+            return;
+        }
+        q.push(s + "0");
+        q.push(s + "9");
+    }
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    
     return 0;
 }
