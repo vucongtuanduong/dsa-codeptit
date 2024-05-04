d06c5db53cc2277b1f617f91425d69204243bdda
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-04 13:33:25 +0000

    redo stack

---
+++ submit/algo-design/stack/dsa07110/dsa07110_2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isOpen(char c) {
+    return (c == '(' || c == '{' || c == '[');
+}
+bool isMatched(char a, char b) {
+    return ((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}'));
+}
+void testCase() {
+    string s;
+    cin >> s;
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++ ){
+        if (isOpen(s[i])) {
+            st.push(s[i]);
+        } else {
+            if (st.empty() || !isMatched(st.top(), s[i])) {
+                cout << "NO";
+                return;
+            } 
+            st.pop();
+        }
+    }
+    cout << "YES";
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
