18316bd9f4ea62f46adf898e81aa4bc222d3d89d
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-04 12:31:20 +0000

    redo stack

---
+++ submit/algo-design/stack/dsa07021/dsa07021_1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    string s;
+    cin >> s;
+    stack<int> st;
+    int res = 0;
+    st.push(-1);
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if (!st.empty()) {
+                res = max(res, i - st.top());
+            } else {
+                st.push(i);
+            }
+        }
+    }
+    cout << res;
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
