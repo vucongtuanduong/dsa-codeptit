a73261fd8ed1fcf1d50f0b4acf983a4a1bc8257b
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-08 02:05:08 +0000

    add 506

--- self/own/sort/506/506.cpp
+++ self/own/sort/506/506.cpp
@@ -2,10 +2,43 @@
 using namespace std;
 class Solution {
 public:
+    static bool cmp(pair<int,int> a, pair<int,int> b) {
+        return a.first > b.first;
+    }
     vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<string> res(n);
+        vector<pair<int,int>> a;
+        for (int i = 0; i < n; i++) {
+            a.push_back({score[i], i});
+        }
+        sort(a.begin(), a.end(), cmp);
+        vector<string> top3 = {"Gold Medal", "Silver Medal", "Bronze Medal"};
         
+        for (int i = 0; i < n; i++) {
+            if (i <= 2) {
+                res[a[i].second] = top3[i];
+            } else {
+                res[a[i].second] = to_string(i + 1);
+            }
+            
+        }
+        return res;
     }
 };
+void testCase() {
+    int n;
+    cin >> n;
+    vector<int> score(n);
+    for (int i = 0; i < n; i++) {
+        cin >> score[i];
+    }
+    Solution h;
+    vector<string> res = h.findRelativeRanks(score);
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";
+    }
+}
 int main() {
     // Write your code here
     int t;
