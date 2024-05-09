ca686266ce6ffe8454161ddb416ab0867a901bf9
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-09 02:02:15 +0000

    3075

--- self/own/greedy/3075/3075.cpp
+++ self/own/greedy/3075/3075.cpp
@@ -3,11 +3,27 @@ using namespace std;
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        long long res = 0;
+        for (int i = 0; i < k; i++){
+            long long temp = happiness[i] - i;
+            if (temp < 0) {
+                temp = 0;
+            }
+            res += temp;
+        }
+        return res;
     }
 };
 void testCase() {
-    
+    int n, k;
+    cin >> n >> k;
+    vector<int> happiness(n);
+    for (int i = 0; i < n; i++) {
+        cin >> happiness[i];
+    }
+    Solution h;
+    cout << h.maximumHappinessSum(happiness, k);
 }
 int main() {
     // Write your code here
