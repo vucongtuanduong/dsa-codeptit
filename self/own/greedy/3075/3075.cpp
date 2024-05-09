66f29836f87039e11b61997f57ef03bab8f45d95
vctduong <165612382+vctduong@users.noreply.github.com>
2024-05-09 01:52:15 +0000

    add 3075

---
+++ self/own/greedy/3075/3075.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        
+    }
+};
+void testCase() {
+    
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
