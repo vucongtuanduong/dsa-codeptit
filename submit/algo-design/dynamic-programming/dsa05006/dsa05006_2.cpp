cbe1fa82216a0693fdc16056733f17b02583afd9
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 15:10:50 +0000

    redo dp

---
+++ submit/algo-design/dynamic-programming/dsa05006/dsa05006_2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=  0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = a[0];
+    for (int i = 1; i < n; i++) {
+        dp[i] = a[i];
+        for (int j = 0; j < i; j++) {
+            if (a[i] > a[j]) {
+                dp[i] = max(dp[i], dp[j] + a[i]);
+            }
+        }
+    }
+    cout << *max_element(dp.begin(), dp.end());
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
