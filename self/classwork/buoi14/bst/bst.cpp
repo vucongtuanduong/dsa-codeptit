8409803f7991c4b7ba86ba55d75ca9c166298996
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 06:30:17 +0000

    edit

--- self/classwork/buoi14/bst/bst.cpp
+++ self/classwork/buoi14/bst/bst.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node (int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
 
+Node *root = NULL;
+
+Node *makeNode(int new_data) {
+    Node *n = new Node(new_data);
+    return n;
+}
 int main() {
     // Write your code here
     return 0;
