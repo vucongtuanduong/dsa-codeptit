6cebe909a723b1c7d6886529e2bdb0639eb831f3
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-10 14:45:37 +0000

    redo dp

---
+++ submit/algo-design/dynamic-programming/dsa05029/dsa05029_3.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (s[i - 1] != '0') {
+            dp[i] += dp[i - 1];
+        }
+        if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
+            dp[i] += dp[i - 2];
+        }
+        
+    }
+    cout << dp[n];
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
