90c499341bf0e30118bcf7b3d12df74e5c9fae64
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-11 11:06:05 +0000

    add files

--- submit/algo-design/bst/dsa11020/README.md
+++ submit/algo-design/bst/dsa11020/README.md
@@ -1,1 +1,2 @@
 ## dsa11020
+![alt text](image.png)
--- submit/algo-design/bst/dsa11020/dsa11020.cpp
+++ submit/algo-design/bst/dsa11020/dsa11020.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testCase() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    set<int> se;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+        se.insert(a[i]);
+    }
+    sort(b.begin(), b.end());
+    if (a == b && se.size() == n) {
+        cout << "1";
+    } else {
+        cout << "0";
+    }
+}
 int main() {
     // Write your code here
-    int 
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
