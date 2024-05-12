a2f006933d5414309756488fc1fd318f61232905
Cong Tuan Duong Vu <82716869+Seragu10@users.noreply.github.com>
2024-05-12 07:46:44 +0000

    add dsa11026

---
+++ submit/algo-design/bst/dsa11026/README.md
@@ -0,0 +1,2 @@
+## dsa11026
+![alt text](image.png)
---
+++ submit/algo-design/bst/dsa11026/dsa11026.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
+Node *buildBalancedBST(int *a, int start, int end) {
+    if (start > end) {
+        return NULL;
+    }
+    int mid = (start + end) / 2;
+    Node *root = new Node(a[mid]);
+    root->left = buildBalancedBST(a, start, mid - 1);
+    root->right = buildBalancedBST(a, mid + 1, end);
+    return root;
+}
+void LRN(Node *root) {
+    if (root != NULL) {
+        LRN(root->left);
+        LRN(root->right);
+        cout << root->data << " ";
+    }
+}
+void testCase(){ 
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i =0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    Node *root = buildBalancedBST(a, 0, n - 1);
+    LRN(root);
+} 
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
