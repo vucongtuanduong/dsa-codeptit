f7482dbce51f2a3e3fb5809afa0c070e9cbf3f38
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-05-23 09:12:50 +0000

    add files

--- self/own/practice/2/loaiBoCapNgoac/loaiBoCapNgoac.cpp
+++ self/own/practice/2/loaiBoCapNgoac/loaiBoCapNgoac.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void testCase() {
+    
+}
 int main() {
     // Write your code here
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        testCase();
+        cout << endl;
+    }
     return 0;
 }
