1a5f9269594007c4726cfedeaa775498f85b79d2
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-07 15:12:21 +0000

    add files

--- self/own/practice/codeforce_956_div2/a/a.cpp
+++ self/own/practice/codeforce_956_div2/a/a.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 using namespace std;
 void generate_beautiful_array(int n) {
-    vector<int> arr(n, 1); // Initialize the array with all elements as 1
-    for (int i = 2; i <= n; i++) {
-        arr[i - 1] = i * i; // Set the ith element to i^2 for each index divisible by i
+    vector<int> a(n + 1, 1); 
+    for (int i = 1; i <= n; i++) {
+        a[i] = i;
     }
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << " ";
     }
     cout << endl;
 }
