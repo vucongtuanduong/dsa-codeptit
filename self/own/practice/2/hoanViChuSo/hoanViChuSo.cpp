f13b88e83674276d01c43011b4cec8720e97a5b2
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-20 07:07:59 +0000

    add files

---
+++ self/own/practice/2/hoanViChuSo/hoanViChuSo.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    return 0;
+}
--- self/own/practice/2/toHopCoThuTuFibonacci/toHopCoThuTuFibonacci.cpp
+++ self/own/practice/2/toHopCoThuTuFibonacci/toHopCoThuTuFibonacci.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int n, k;
 long long fibo[93] = {0};
+vector<int> a;
+int cnt = 2;
+int stt = 0;
 void init() {
     fibo[0] = 0;
     fibo[1] = 1;
@@ -9,7 +12,35 @@ void init() {
         fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
 }
+void result() {
+    stt++;
+    if (stt == fibo[cnt]) {
+        cnt++;
+        cout << stt << ": ";
+        for (int i = 1; i <= k; i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+}
+void Try(int i) {
+    for (int j = a[i - 1] + 1; j <= (n - k + i); j++) {
+        a[i] = j;
+        if (i == k) {
+            result();
+        } else {
+            Try(i + 1);
+        }
+    }
+}
 int main() {
     // Write your code here
+    init();
+    cin >> n >> k;
+    a.resize(k + 1);
+    a[0] = 0;
+    Try(1);
     return 0;
 }
