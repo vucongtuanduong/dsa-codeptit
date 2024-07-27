8ede7a4d35c143b6b7099f5cd795c9e4efd1e536
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-27 07:46:02 +0000

    add files

--- self/own/practice/2/danhSoThuTuCapDauNgoac/danhSoThuTuCapDauNgoac.cpp
+++ self/own/practice/2/danhSoThuTuCapDauNgoac/danhSoThuTuCapDauNgoac.cpp
@@ -3,7 +3,21 @@ using namespace std;
 void testCase() {
     string s;
     getline(cin, s);
-    cout << s;
+    stack<int> st;
+    int count = 1;
+    vector<int> res;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            res.push_back(count);
+            st.push(count++);
+        } else if (!st.empty() && s[i] == ')') {
+            res.push_back(st.top());
+            st.pop();
+        }
+    }
+    for (int x : res) {
+        cout << x << " ";
+    }
 }
 int main() {
     // Write your code here
