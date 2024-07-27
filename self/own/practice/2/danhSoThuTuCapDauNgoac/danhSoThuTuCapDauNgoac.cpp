9b21a776a6f4a1637bfb658c80b8996dc7830d98
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-27 07:29:58 +0000

    add files

---
+++ self/own/practice/2/danhSoThuTuCapDauNgoac/danhSoThuTuCapDauNgoac.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void testCase() {
+    string s;
+    getline(cin, s);
+    cout << s;
+}
+int main() {
+    // Write your code here
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--) {
+        testCase();
+        cout << endl;
+    }
+    return 0;
+}
