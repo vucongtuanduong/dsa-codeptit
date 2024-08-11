cdc416529607ceabaec9184bfc8a815d7429e0d5
vctduong <165612382+vctduong@users.noreply.github.com>
2024-08-11 04:56:52 +0000

    add files

--- self/own/practice/icpc_4/4I/4I.cpp
+++ self/own/practice/icpc_4/4I/4I.cpp
@@ -17,12 +17,37 @@ int main() {
     // // cout << endl;
     // cout << res;
 
+    if (k == 0) {
+        long long res = n * n;
+        cout << res;
+        return 0;
+    }
     long long x = n - k;
     long long res = (x * (x + 1)) / 2;
     for (long long i = k; i <= n; i++) {
-        long long temp = (n - k) / (i + 1);
-        res += temp;
+        int j = i + 1;
+        while (j <= (n - i)) {
+            
+            long long temp = (n - i) / (j);
+            cout << "i: " << i << ", j: " << j << ", temp: " << temp << endl;
+            res += temp;
+            j++;
+        }
+        // long long temp = (n - k) / (i + 1);
+        // res += temp;
     }
     cout << res;
+
+
+    // long long count = 0;
+    // for (long long i = 1; i <= n; i++) {
+    //     for (long long j = 1; j <= n; j++) {
+    //         if (i % j >= k) {
+    //             cout << i << " " << j << endl;
+    //             count++;
+    //         }
+    //     }
+    // }
+    // cout << endl << count << endl;
     return 0;
 }
